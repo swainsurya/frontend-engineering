1e10a676d93bf7bc5e22cf1628dd4519b62ae5df
surya <swainsuryakanta97@gmail.com>
2025-07-11 20:36:47 +0530

    HEADER SECTION IN PROGRESS

---
+++ netflix/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full h-screen bg-red-400 relative' style={{backgroundImage: "url(/netflix_home.jpg)"}}>
+      <div className='w-full h-screen bg-black absolute top-0 left-0 opacity-60' />
+      <div className='max-w-7xl h-screen border-4 border-white mx-auto '>
+        {/* navbar */}
+        {/* header hero */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
